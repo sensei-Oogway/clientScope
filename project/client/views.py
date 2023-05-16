76054e8a86377a0856c25f96bcae6cb2ca01c80a
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2023-05-16 17:11:06 +1000

    static files collected + index page html added

--- project/client/views.py
+++ project/client/views.py
@@ -3,3 +3,6 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request):
     return render(request,"index.html")
+
+def home(request):
+    return HttpResponse("homePage")
--- project/project/urls.py
+++ project/project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , include("client.urls")),
     path('client/' , include("client.urls")),
     #path('professional/', include("professional.urls"))
 ]
