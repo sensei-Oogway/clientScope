3fec286e0887d70694e38075d2532dcdfd89c4fb
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2023-05-17 14:56:30 +1000

    added common login - and basic changes

--- project/client/views.py
+++ project/client/views.py
@@ -4,5 +4,13 @@ from django.http import HttpRequest, HttpResponse
 def index(request):
     return render(request,"index.html")
 
+def commonLogin(request):
+    emailId = request.POST.get('email')
+    passWord = request.POST.get('password')
+
+    print(emailId, passWord)
+
+    return HttpResponse("login successful")
+
 def home(request):
     return HttpResponse("homePage")
