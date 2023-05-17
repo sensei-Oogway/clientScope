b506e946df7d1ac762239827265b1454e2145b7c
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2023-05-17 14:55:19 +1000

    added a dummy Util folder and  constant Converter

--- project/client/urls.py
+++ project/client/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [path("",views.index),path("home",views.home)]
+urlpatterns = [path("",views.index),path("home",views.home), path("login",views.commonLogin)]
---
+++ project/utils/constant_converter.py
@@ -0,0 +1,3 @@
+class ConstantsClass:
+    def getServiceID_forName(serviceName):
+        serviceNames = ["sdfg","sdfgf","sdfgsfdg","sdfg"]
